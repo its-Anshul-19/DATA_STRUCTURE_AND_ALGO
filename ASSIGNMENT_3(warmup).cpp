1e816a74fc1368b1ecf1d797538deb39c6f7f0e1
its-Anshul-18 <108014798+its-Anshul-18@users.noreply.github.com>
2022-10-07 22:42:46 +0530

    Create ASSIGNMENT_3(warmup).cpp

---
+++ ASSIGNMENT_3(warmup).cpp
@@ -0,0 +1,25 @@
+QUESTION-SOLVE_ME_FIRST.
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+/*int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  
+}*/
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = num1+num2;
+  cout<<sum;
+  return 0;
+}
+---------------------------------------------------------------------------------------------------------
+ QUESTION- SIMPLE ARRAY SUM.
+ 
